ef2e2a84ed39184e0011c03b829f0a03032490a0
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-12-22 19:27:43 +0300

    created Main.java

---
+++ src/ru/serjeypyzin/javacore/sample/Main.java
@@ -0,0 +1,11 @@
+package ru.serjeypyzin.javacore.sample;
+
+import ru.serjeypyzin.javacore.regular.UserInteraction;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInteraction userInteraction = new UserInteraction();
+        userInteraction.menu();
+
+    }
+}
